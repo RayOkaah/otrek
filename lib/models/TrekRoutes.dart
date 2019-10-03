105337ea0bfd53d1a1eacb32e5d82ba1ed9fa4e5
Ray Okaah <raypex2@gmail.com>
2019-10-03 18:35:14 +0100

    Finish Trek Routes Ui

---
+++ lib/models/TrekRoutes.dart
@@ -0,0 +1,26 @@
+class TrekRoutes {
+  Map<String, dynamic> dummyRoutesList = {
+    "responseCode": "1",
+    "responseText": "List categories.",
+    "responseBody": [
+      {"route_id": "1", "route_start": "BoriCamp", "route_end": "Choba"},
+      {"route_id": "2", "route_start": "Choba", "route_end": "Alakahia"},
+      {"route_id": "3", "route_start": "Cook", "route_end": "Sleep"},
+      {"route_id": "4", "route_start": "Choba", "route_end": "Alakahia"},
+      {"route_id": "5", "route_start": "Mile3", "route_end": "Mile2"},
+      {"route_id": "6", "route_start": "Choba", "route_end": "Alakahia"},
+      {"route_id": "7", "route_start": "Choba", "route_end": "Alakahia"},
+      {"route_id": "8", "route_start": "Choba", "route_end": "Alakahia"},
+      {"route_id": "9", "route_start": "Choba", "route_end": "Alakahia"},
+      {"route_id": "10", "route_start": "Choba", "route_end": "Alakahia"},
+      {"route_id": "11", "route_start": "Choba", "route_end": "Alakahia"},
+      {"route_id": "12", "route_start": "Choba", "route_end": "Alakahia"},
+      {"route_id": "13", "route_start": "Choba", "route_end": "Alakahia"},
+      {"route_id": "14", "route_start": "Choba", "route_end": "Alakahia"},
+      {"route_id": "15", "route_start": "Choba", "route_end": "Alakahia"},
+      {"route_id": "16", "route_start": "Choba", "route_end": "Alakahia"},
+    ],
+    "responseTotalResult":
+        16 // Total result is 3 here becasue we have 3 categories in responseBody.
+  };
+}
--- lib/screens/TrekRoutesScreen.dart
+++ lib/screens/TrekRoutesScreen.dart
@@ -1,4 +1,6 @@
+import 'package:floating_search_bar/floating_search_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:otrek/models/TrekRoutes.dart';
 
 class TrekRoutesScreen extends StatefulWidget {
   @override
@@ -11,31 +13,8 @@ class TrekRoutesScreenState extends State<TrekRoutesScreen> {
   List _selectedRoutes = List();
   int _maxAllowed = 5;
   bool _isCheckEnabled = true;
+  Map<String, dynamic> _routes = TrekRoutes().dummyRoutesList;
 
-  Map<String, dynamic> _routes = {
-    "responseCode": "1",
-    "responseText": "List categories.",
-    "responseBody": [
-      {"route_id": "1", "route_start": "BoriCamp", "route_end": "Choba"},
-      {"route_id": "2", "route_start": "Choba", "route_end": "Alakahia"},
-      {"route_id": "3", "route_start": "Cook", "route_end": "Sleep"},
-      {"route_id": "4", "route_start": "Choba", "route_end": "Alakahia"},
-      {"route_id": "5", "route_start": "Choba", "route_end": "Alakahia"},
-      {"route_id": "6", "route_start": "Choba", "route_end": "Alakahia"},
-      {"route_id": "7", "route_start": "Choba", "route_end": "Alakahia"},
-      {"route_id": "8", "route_start": "Choba", "route_end": "Alakahia"},
-      {"route_id": "9", "route_start": "Choba", "route_end": "Alakahia"},
-      {"route_id": "10", "route_start": "Choba", "route_end": "Alakahia"},
-      {"route_id": "11", "route_start": "Choba", "route_end": "Alakahia"},
-      {"route_id": "12", "route_start": "Choba", "route_end": "Alakahia"},
-      {"route_id": "13", "route_start": "Choba", "route_end": "Alakahia"},
-      {"route_id": "14", "route_start": "Choba", "route_end": "Alakahia"},
-      {"route_id": "15", "route_start": "Choba", "route_end": "Alakahia"},
-      {"route_id": "16", "route_start": "Choba", "route_end": "Alakahia"},
-    ],
-    "responseTotalResult":
-        16 // Total result is 3 here becasue we have 3 categories in responseBody.
-  };
 
   void _onCategorySelected(bool selected, category_id) {
     if (_selectedRoutes.length < _maxAllowed) {
@@ -66,42 +45,149 @@ class TrekRoutesScreenState extends State<TrekRoutesScreen> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("go"),
-      ),
-      body: ListView.builder(
-          itemCount: _routes['responseTotalResult'],
-          itemBuilder: (BuildContext context, int index) {
-            return Row(
-              children: <Widget>[
-                Container(
-                  child: Checkbox(
-                    tristate: true,
-                    value: (_selectedRoutes.length > _maxAllowed)
-                        ? () => null
-                        : _selectedRoutes.contains(
-                            _routes['responseBody'][index]['route_id']),
-                    onChanged: _isCheckEnabled
-                        ? (bool selected) {
+      body: Column(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(top: 40),
+            child: ListTile(
+              title: Text("Trek Routes",
+                style: TextStyle(
+                    fontFamily: "Popins",
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
+                    color: Color(0xff707070)
+                ),
+              ),
+              subtitle: Text("Port Harcourt",
+                style: TextStyle(
+                    fontFamily: "Lato",
+                    fontSize: 16,
+                    color: Color(0xff707070)
+                ),
+              ),
+              trailing: Container(
+                child: RaisedButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(5))),
+                    textColor: Colors.white,
+                    color: Color(0xff15A4D1),
+                    child: Text(
+                      "Save",
+                      style: TextStyle(
+                        fontFamily: "Lato",
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                    onPressed: (_selectedRoutes.length < _maxAllowed - 2) ?
+                    null : () {}
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: FloatingSearchBar.builder(
+              itemCount: _routes['responseTotalResult'],
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        child: Checkbox(
+                          tristate: true,
+                          value: (_selectedRoutes.length > _maxAllowed)
+                              ? () => null
+                              : _selectedRoutes.contains(
+                              _routes['responseBody'][index]['route_id']),
+                          onChanged: _isCheckEnabled
+                              ? (bool selected) {
                             _onCategorySelected(selected,
                                 _routes['responseBody'][index]['route_id']);
                             print(_selectedRoutes.length.toString());
                             print(_selectedRoutes.toString());
                           }
-                        : null,
-                    //title: Text(_routes['responseBody'][index]['category_name']),
+                              : null,
+                          //title: Text(_routes['responseBody'][index]['category_name']),
+                        ),
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Text(_routes['responseBody'][index]['route_start'],
+                            style: TextStyle(fontFamily: "Lato",
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xff707070)),
+                          ),
+                          Icon(Icons.keyboard_arrow_right, size: 15,),
+                          Text(_routes['responseBody'][index]['route_end'],
+                            style: TextStyle(fontFamily: "Lato",
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xff707070)),
+                          )
+                        ],
+                      )
+                    ],
                   ),
-                ),
-                Row(
-                  children: <Widget>[
-                    Text(_routes['responseBody'][index]['route_start']),
-                    Icon(Icons.keyboard_arrow_right),
-                    Text(_routes['responseBody'][index]['route_end'])
-                  ],
-                )
+                );
+              },
+              trailing: Icon(Icons.search, color: Color(0xff15A4D1),),
+
+              /**
+                  decoration: InputDecoration.collapsed(
+                  //hintText: "",
+                  focusColor: Color(0xff15A4D1),
+                  hoverColor: Color(0xff15A4D1),
+
+                  ),
+               */
+            ),
+          ),
+
+
+          /**
+              Expanded(
+              flex: 1,
+              child: ListView.builder(
+              itemCount: _routes['responseTotalResult'],
+              itemBuilder: (BuildContext context, int index) {
+              return Row(
+              children: <Widget>[
+              Container(
+              child: Checkbox(
+              tristate: true,
+              value: (_selectedRoutes.length > _maxAllowed)
+              ? () => null
+              : _selectedRoutes.contains(
+              _routes['responseBody'][index]['route_id']),
+              onChanged: _isCheckEnabled
+              ? (bool selected) {
+              _onCategorySelected(selected,
+              _routes['responseBody'][index]['route_id']);
+              print(_selectedRoutes.length.toString());
+              print(_selectedRoutes.toString());
+              }
+              : null,
+              //title: Text(_routes['responseBody'][index]['category_name']),
+              ),
+              ),
+              Row(
+              children: <Widget>[
+              Text(_routes['responseBody'][index]['route_start'],
+              style: TextStyle(fontFamily: "Lato", fontWeight: FontWeight.bold, color: Color(0xff707070)),
+              ),
+              Icon(Icons.keyboard_arrow_right, size: 15,),
+              Text(_routes['responseBody'][index]['route_end'],
+              style: TextStyle(fontFamily: "Lato", fontWeight: FontWeight.bold, color: Color(0xff707070)),
+              )
               ],
-            );
-          }),
+              )
+              ],
+              );
+              }),
+              ),
+
+           */
+        ],
+      ),
     );
   }
 }
